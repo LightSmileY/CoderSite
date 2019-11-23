32b3ab3155068eae6b18619a0bc3620c247387d1
袁乾峰 <2434740987@qq.com>
2019-11-23 22:59:25 +0800

    完成消息列表组件

--- src/components/messageList.vue
+++ src/components/messageList.vue
@@ -1,14 +1,50 @@
 <template>
-  <div id="messageList">
-    messageList
-  </div>
+  <ul id="messageList">
+    <li v-for="item in 5">
+      <div class="user">
+        <div class="user-info">
+          <div class="avatar">
+            
+          </div>
+          <div class="name-time">
+            <div class="name">
+              浅笑半离兮
+            </div>
+            <div class="time">
+              2019-11-23 21:56:30
+            </div>
+          </div>
+        </div>
+        <div class="attent">
+          <van-button 
+          plain 
+          hairline 
+          round
+          color="#939393"
+          type="default" 
+          size="mini">+关注</van-button>
+        </div>
+      </div>
+      <div class="message-type">
+        收藏了你的文章
+      </div>
+      <div class="detail">
+        <div class="image">
+          
+        </div>
+        <div class="desc">
+          哈哈哈哈哈哈哈哈哈哈或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或
+        </div>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <script>
   export default{
     data(){
       return{
-        value: ""
+        
       }
     },
     props: {
