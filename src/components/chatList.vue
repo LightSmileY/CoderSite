373c9da6f0daa74decaa732e291de101ba30c5b8
袁乾峰 <2434740987@qq.com>
2019-11-23 23:00:29 +0800

    创建了聊天列表组件

---
+++ src/components/chatList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="chatList">
+    chatList
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return{
+        value: ""
+      }
+    },
+    props: {
+      
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import '@/assets/scss/components/chatList';
+</style>
