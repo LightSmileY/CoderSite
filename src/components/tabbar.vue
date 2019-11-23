58e4be28804ec6d70f1a15c0f1913818c7a30cd6
袁乾峰 <2434740987@qq.com>
2019-11-23 16:18:32 +0800

    完善tabbar

--- src/components/tabbar.vue
+++ src/components/tabbar.vue
@@ -1,10 +1,30 @@
 <template>
   <ul id="tabbar">
-    <li><img src="@/assets/icons/index.png" alt=""></li>
-    <li><img src="@/assets/icons/find.png" alt=""></li>
-    <li><img src="@/assets/icons/publish.png" alt=""></li>
-    <li><img src="@/assets/icons/message.png" alt=""></li>
-    <li><img src="@/assets/icons/mine.png" alt=""></li>
+    <li>
+      <router-link to="/">
+        <img src="@/assets/icons/index.png" alt="">
+      </router-link>
+    </li>
+    <li>
+      <router-link to="/find">
+        <img src="@/assets/icons/find.png" alt="">
+      </router-link>
+    </li>
+    <li>
+      <router-link to="/publish">
+        <img src="@/assets/icons/publish.png" alt="">
+      </router-link>
+    </li>
+    <li>
+      <router-link to="/message">
+        <img src="@/assets/icons/message.png" alt="">
+      </router-link>
+    </li>
+    <li>
+      <router-link to="/mine">
+        <img src="@/assets/icons/mine.png" alt="">
+      </router-link>
+    </li>
   </ul>
 </template>
 
