45da68234be1335f16ae82b51bdd6e7ffa70da69
袁乾峰 <2434740987@qq.com>
2019-11-23 17:51:25 +0800

    搭建了消息页基本结构

--- src/views/Message/index.vue
+++ src/views/Message/index.vue
@@ -1,21 +1,70 @@
 <template>
   <div id="message">
-    message
+    <van-nav-bar
+      fixed="true"
+      title="标题"
+      left-text="返回"
+      right-text="按钮"
+      left-arrow
+      @click-left=""
+      @click-right=""
+    />
+    <div class="main">
+      <!-- 标签页 -->
+      <van-tabs 
+      v-model="active" 
+      swipeable
+      sticky="true"
+      title-active-color="#90C2EF">
+        <van-tab 
+        title="选项"
+        >
+        <!-- 下拉刷新 -->
+          <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+            <div class="message-list">1</div>
+          </van-pull-refresh>
+        </van-tab>
+        <van-tab 
+        title="选项"
+        >
+          <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+            <div class="answer-list">2</div>
+          </van-pull-refresh>
+        </van-tab>
+        <van-tab 
+        title="选项"
+        >
+          <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+            <div class="chat-list">3</div>
+          </van-pull-refresh>
+        </van-tab>
+      </van-tabs>
+    </div>
     <tab-bar/>
   </div>
 </template>
 
 <script>
   import TabBar from '@/components/tabbar'
+  import Header from '@/components/header'
 
   export default {
     data(){
       return {
-
+        isLoading: false
       }
     },
     components: {
-      TabBar
+      TabBar,
+      Header
+    },
+    methods: {
+      onRefresh() {
+        setTimeout(() => {
+          this.$toast('刷新成功')
+          this.isLoading = false
+        }, 500)
+      }
     }
   };
 </script>
