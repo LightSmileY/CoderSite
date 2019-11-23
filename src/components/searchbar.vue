47aecc4373fcd0135e428921e7a110e7c9d19f4f
袁乾峰 <2434740987@qq.com>
2019-11-23 15:10:21 +0800

    添加了搜索栏组件

---
+++ src/components/searchbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="searchbar">
+    <div class="box">
+      <van-search
+        v-model="value"
+        placeholder="搜索你想要的内容···"
+        show-action
+        shape="round"
+        @search="onSearch"
+      >
+        <div slot="action" @click="onSearch">搜索</div>
+      </van-search>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return{
+        value: ""
+      }
+    },
+    methods: {
+      onSearch(){
+        
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import '@/assets/scss/components/searchbar';
+</style>
