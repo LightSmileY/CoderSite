882b338f02a178a2b2e87ad3723ad84b2710f78b
袁乾峰 <2434740987@qq.com>
2019-11-23 15:09:42 +0800

    完成了消息列表组件

---
+++ src/components/messageList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="messageList">
+    messageList
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return{
+        value: ""
+      }
+    },
+    props: {
+      
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import '@/assets/scss/components/messageList';
+</style>
