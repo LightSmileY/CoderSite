14147f4242494ec443684721204a6efb18e56ce2
袁乾峰 <2434740987@qq.com>
2019-11-23 23:00:10 +0800

    创建了回答列表组件

---
+++ src/components/answerList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="answerList">
+    answerList
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return{
+        value: ""
+      }
+    },
+    props: {
+      
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import '@/assets/scss/components/answerList';
+</style>
