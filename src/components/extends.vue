5bbda7b604acd272eb81620554116d4edf0dbed8
袁乾峰 <2434740987@qq.com>
2019-11-23 15:09:11 +0800

    添加了扩展功能列表组件

---
+++ src/components/extends.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul id="extends">
+    <li v-for="item in 8">
+      <div class="image">
+        <img src="@/assets/icons/index.png" alt="">
+      </div>
+      <div class="label">功能一</div>
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import '@/assets/scss/components/extends';
+</style>
