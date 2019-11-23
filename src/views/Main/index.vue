eb8c54b27575052e2d75d280180b4fcb72ca2ae2
袁乾峰 <2434740987@qq.com>
2019-11-23 17:51:01 +0800

    还原主页结构

--- src/views/Main/index.vue
+++ src/views/Main/index.vue
@@ -1,22 +1,61 @@
 <template>
   <div id="main">
-    main
+    <search-bar/>
+    <van-pull-refresh class="container" v-model="isLoading" @refresh="onRefresh">
+      <div class="swiper">
+        <van-swipe style="height: 130px;" :autoplay="3000">
+          <van-swipe-item v-for="(image, index) in images" :key="index">
+            <van-image :src="image" height="100%" fit="cover"/>
+          </van-swipe-item>
+        </van-swipe>
+      </div>
+      <div class="extends">
+        <Extends/>
+      </div>
+      <div class="hot">
+        <div class="title">猜你喜欢</div>
+        <post-list/>
+      </div>
+      <div class="youlike">
+        <div class="title">热门文章</div>
+        <post-list/>
+      </div>
+    </van-pull-refresh>
     <tab-bar/>
   </div>
 </template>
 
 <script>
   import TabBar from '@/components/tabbar'
+  import SearchBar from '@/components/searchbar'
+  import Extends from '@/components/extends'
+  import PostList from '@/components/postList'
 
   export default {
     data(){
       return {
-
+        images: [
+          "http://tva1.sinaimg.cn/large/0060lm7Tly1g64rb3s5djj32f80rs7gw.jpg",
+          "http://tva1.sinaimg.cn/large/0060lm7Tly1g64rb3s5djj32f80rs7gw.jpg",
+          "http://tva1.sinaimg.cn/large/0060lm7Tly1g64rb3s5djj32f80rs7gw.jpg"
+        ],
+        isLoading: false
       }
     },
     components: {
-      TabBar
+      TabBar,
+      SearchBar,
+      Extends,
+      PostList
+    },
+    methods: {
+    onRefresh() {
+      setTimeout(() => {
+        this.$toast('刷新成功')
+        this.isLoading = false
+      }, 500)
     }
+  }
   };
 </script>
 
