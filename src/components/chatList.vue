504c624d91f98e85c28cded0a831d91625f4d86c
袁乾峰 <2434740987@qq.com>
2019-11-25 11:30:43 +0800

    完成私聊列表组件

--- src/components/chatList.vue
+++ src/components/chatList.vue
@@ -1,7 +1,24 @@
 <template>
-  <div id="chatList">
-    chatList
-  </div>
+  <ul id="chatList">
+    <li v-for="item in 5">
+      <div class="infos">
+        <div class="avatar">
+          
+        </div>
+        <div class="name-message">
+          <div class="name">
+            浅笑半离兮
+          </div>
+          <div class="message">
+            我推荐你用element-ui
+          </div>
+        </div>
+      </div>
+      <div class="tag">
+        <van-tag round type="danger">5</van-tag>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <script>
