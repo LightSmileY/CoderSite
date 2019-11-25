ed546e0cdc574ae1e175cb1778fe158f9befbb84
袁乾峰 <2434740987@qq.com>
2019-11-25 22:03:07 +0800

    完成发现页

--- src/views/Find/index.vue
+++ src/views/Find/index.vue
@@ -1,14 +1,32 @@
 <template>
   <div id="find">
-    <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
-      
-    </van-pull-refresh>
+    <!-- 发现页头部 -->
+    <div class="header">
+      <div class="navs">
+        <li>推荐</li>
+        <li class="li-active">关注</li>
+        <li>最新</li>
+        <li>热门</li>
+        <li>问题</li>
+      </div>
+      <div class="search">
+        <van-icon name="search" size="25px"/>
+      </div>
+    </div>
+    <div class="container">
+      <div class="articleList">
+        <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+          <article-list/>
+        </van-pull-refresh>
+      </div>
+    </div>
     <tab-bar/>
   </div>
 </template>
 
 <script>
   import TabBar from '@/components/tabbar'
+  import ArticleList from '@/components/articleList'
 
   export default {
     data(){
@@ -17,7 +35,8 @@
       }
     },
     components: {
-      TabBar
+      TabBar,
+      ArticleList
     },
     methods: {
       onRefresh() {
