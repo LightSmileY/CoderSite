0b11ef97d47c468a67383e95724347b7fab2e78b
袁乾峰 <2434740987@qq.com>
2019-11-27 21:19:26 +0800

    创建了文章/问题评论组件

---
+++ src/components/commentList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="commentList">
+    commentList
+  </div>
+</template>
+
+<script>
+
+  export default {
+    data(){
+      return {
+
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import '@/assets/scss/components/commentList';
+</style>
