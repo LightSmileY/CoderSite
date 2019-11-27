788311165f7058536411b74208e99995bb9e5910
袁乾峰 <2434740987@qq.com>
2019-11-27 21:17:59 +0800

    调整帖子列表的点赞、收藏、转发

--- src/components/postList.vue
+++ src/components/postList.vue
@@ -1,6 +1,6 @@
 <template>
   <ul id="postList">
-    <li v-for="item in 5">
+    <li v-for="item in 5" @click="toArticleDetailPage(123)">
       <!-- 左边图片 -->
       <div class="image">
         <!-- van图片 -->
@@ -18,26 +18,26 @@
         <!-- 文章首段 -->
         <div class="desc">哈哈哈哈哈哈哈哈哈哈或或或或或或或或或或或或或或或或或或或或或哈哈哈哈哈哈哈哈哈哈或或或或或或或或或或或或或或</div>
         <!-- 点赞、评论、收藏信息 -->
-        <div class="operinfo">
-          <div class="item">
+        <ul class="operinfo">
+          <li class="item">
             <div class="icon">
               <img src="@/assets/icons/index.png" alt="">
             </div>
-            <div class="label">喜欢</div>
-          </div>
-          <div class="item">
+            <div class="label">234</div>
+          </li>
+          <li class="item">
             <div class="icon">
               <img src="@/assets/icons/index.png" alt="">
             </div>
-            <div class="label">收藏</div>
-          </div>
-          <div class="item">
+            <div class="label">335</div>
+          </li>
+          <li class="item">
             <div class="icon">
               <img src="@/assets/icons/index.png" alt="">
             </div>
-            <div class="label">评论</div>
-          </div>
-        </div>
+            <div class="label">124</div>
+          </li>
+        </ul>
       </div>
     </li>
   </ul>
@@ -48,6 +48,15 @@
     data(){
       return {
 
+      }
+    },
+    methods: {
+      // 去文章详情页
+      toArticleDetailPage(i){
+        this.$router.push({
+          name:'articleDetail',
+          query: { id: i }
+        })
       }
     }
   };
