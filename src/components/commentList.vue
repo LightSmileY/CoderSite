89054444e04d06ce9597d33056d28c4075d1565b
袁乾峰 <2434740987@qq.com>
2019-11-27 22:30:59 +0800

    完成评论列表组件

--- src/components/commentList.vue
+++ src/components/commentList.vue
@@ -1,6 +1,29 @@
 <template>
   <div id="commentList">
-    commentList
+    <li v-for="item in 5">
+      <div class="avatar">
+        <!-- van图片 -->
+        <van-image
+          lazy-load
+          fit="cover"
+          src="http://cdn.fengblog.xyz/avatar.jpg">
+          <template v-slot:loading>
+            <van-loading type="spinner" size="20" />
+          </template>
+          <template v-slot:error>加载失败</template>
+        </van-image>
+      </div>
+      <div class="main">
+        <div class="name">浅笑半离兮</div>
+        <div class="content">
+          哈哈哈哈哈哈哈哈哈哈或或或或或或或或或或或或或或或或或或或或或或或
+        </div>
+        <div class="info">
+          <div class="info-time">35分钟前</div>
+          <div class="info-reply">回复</div>
+        </div>
+      </div>
+    </li>
   </div>
 </template>
 
