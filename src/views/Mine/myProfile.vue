0ed1044a30139bf7e25a979e1ef21d102907f8e0
袁乾峰~lab <2434740987@qq.com>
2019-11-30 14:55:33 +0800

    个人资料页引入部分vant组件

--- src/views/Mine/myProfile.vue
+++ src/views/Mine/myProfile.vue
@@ -1,6 +1,61 @@
 <template>
   <div id="myProfile">
-    myProfile
+    <van-nav-bar
+      fixed="true"
+      title="资料"
+      right-text="更多"
+      @click-right=""
+    />
+    <div class="container">
+      <!-- <van-cell-group>
+        <van-field
+          v-model="username"
+          clearable
+          label="用户名"
+          placeholder="lightsmiley"
+          disabled
+        />
+        <van-field
+          v-model="password"
+          type="password"
+          label="昵称"
+          placeholder="请输入昵称"
+        />
+        <van-field
+          v-model="username"
+          clearable
+          label="生日"
+          placeholder="请选择生日"
+        />
+        <!-- 选择时间 -->
+        <!-- <van-dialog
+          v-model="show"
+          :close-on-popstate="true"
+          :close-on-click-overlay="true"
+        >
+          <van-datetime-picker
+            v-model="currentDate"
+            type="date"
+            :min-date="minDate"
+            :show-toolbar="false"
+            :item-height="35"
+          />
+        </van-dialog>
+        
+        <van-field
+          v-model="password"
+          type="password"
+          label="邮箱"
+          placeholder="请输入邮箱"
+        />
+        <van-field
+          v-model="username"
+          clearable
+          label="个性签名"
+          placeholder="请输入个性签名"
+        />
+      </van-cell-group> -->
+    </div>
   </div>
 </template>
 
@@ -9,7 +64,7 @@
   export default {
     data(){
       return {
-
+        show: true
       }
     }
   };
