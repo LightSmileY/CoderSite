343a245c39912ddb3d634958f244c9a6c5bbfb8d
袁乾峰~lab <2434740987@qq.com>
2019-11-30 14:54:47 +0800

    完成发表过程样式

--- src/components/tabbar.vue
+++ src/components/tabbar.vue
@@ -10,8 +10,23 @@
         <img src="@/assets/icons/find.png" alt="">
       </router-link>
     </li>
-    <li>
-      <img src="@/assets/icons/publish.png" alt="">
+    <li @click="showpub = !showpub">
+      <img src="@/assets/icons/publish.png" alt="" v-if="!showpub">
+      <img src="@/assets/icons/delete.png" alt="" v-if="showpub">
+      <div class="publish" v-if="showpub">
+        <dl @click.stop="toPubArticlePage">
+          <dt>
+            <img src="@/assets/icons/post.png" alt="">
+          </dt>
+          <dd>发帖子</dd>
+        </dl>
+        <dl @click.stop="toPubQuestionPage">
+          <dt>
+            <img src="@/assets/icons/question.png" alt="">
+          </dt>
+          <dd>提问题</dd>
+        </dl>
+      </div>
     </li>
     <li>
       <router-link to="/message">
@@ -25,6 +40,28 @@
     </li>
   </ul>
 </template>
+
+<script>
+  export default {
+    data(){
+      return{
+        showpub: false
+      }
+    },
+    methods: {
+      toPubArticlePage(){
+        this.$router.push({
+          name:'pubArticle'
+        })
+      },
+      toPubQuestionPage(){
+        this.$router.push({
+          name:'pubQuestion'
+        })
+      }
+    }
+  };
+</script>
 
 <style lang="scss" scoped>
   @import '@/assets/scss/components/tabbar.scss';
