85a12bcc32adbdafa42f1556fbc33c9a91df3d57
袁乾峰 <2434740987@qq.com>
2019-12-03 14:00:48 +0800

    完成文章发表页

--- src/views/Publish/pubArticle.vue
+++ src/views/Publish/pubArticle.vue
@@ -1,6 +1,47 @@
 <template>
   <div id="pubArticle">
-    pubArticle
+    <van-nav-bar
+      fixed="true"
+      z-index="1000"
+      title="发表文章"
+      left-arrow
+      @click-left="goback"
+    />
+    <div class="container">
+      <van-field
+        v-model="message"
+        rows="1"
+        autosize
+        label="话题"
+        placeholder="请输入话题"
+        clearable
+        label-width="60px"
+      />
+      <!-- 段落 -->
+      <div class="para" v-for="item in 1">
+        <van-field
+          v-model="message"
+          rows="1"
+          autosize
+          :label="'段落'+para"
+          type="textarea"
+          placeholder="请输入内容"
+          clearable
+          label-width="60px"
+        />
+        <div class="uploadImage">
+          <van-uploader v-model="imageList" multiple />
+        </div>
+      </div>
+      <!-- 添加段落 -->
+      <div class="addPara">
+        <van-button plain hairline icon="plus" type="primary">添加段落</van-button>
+      </div>
+      <!-- 发表帖子 -->
+      <div class="post">
+        <van-button type="primary" size="large">发表</van-button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,7 +50,14 @@
   export default {
     data(){
       return {
-
+        message: "",
+        para: 1,
+        imageList: []
+      }
+    },
+    methods: {
+      goback(){
+        this.$router.go(-1)
       }
     }
   };
