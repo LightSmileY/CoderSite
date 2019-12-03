c0d1c10b44d1420f5c331c0004cef45090c0d717
袁乾峰 <2434740987@qq.com>
2019-12-03 14:00:27 +0800

    添加了animate.css库和瀑布流组件

--- src/main.js
+++ src/main.js
@@ -6,12 +6,15 @@ import Vant from 'vant'
 import VueLazyload from 'vue-lazyload'
 import '@/config/rem'
 import 'vant/lib/index.css'
-
+import animated from 'animate.css'
+import waterfall from 'vue-waterfall2'
 
 Vue.config.productionTip = false
 
 Vue.use(Vant)
 Vue.use(VueLazyload)
+Vue.use(animated)
+Vue.use(waterfall)
 
 new Vue({
   router,
