db2f4a455c2e1d20d9e11191074d9a063366c636
袁乾峰 <2434740987@qq.com>
2019-12-04 09:09:02 +0800

    添加用户列表组件

---
+++ src/components/userList.vue
@@ -0,0 +1,58 @@
+<template>
+  <ul id="userList">
+    <li v-for="item in 15" @click="toUserProfile(123)">
+      <div class="infos">
+        <div class="avatar">
+          <!-- van图片 -->
+          <van-image
+            lazy-load
+            fit="cover"
+            src="http://cdn.fengblog.xyz/avatar.jpg">
+            <template v-slot:loading>
+              <van-loading type="spinner" size="20" />
+            </template>
+            <template v-slot:error>加载失败</template>
+          </van-image>
+        </div>
+        <div class="name-message">
+          <div class="name">
+            浅笑半离兮
+          </div>
+          <div class="signiture">
+            我推荐你用element-ui
+          </div>
+        </div>
+      </div>
+      <div class="attent">
+        <van-button v-if="false" type="primary" color="#47CE8E" size="mini">已关注</van-button>
+        <van-button type="primary" color="#47CE8E" size="mini">关注TA</van-button>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default{
+    data(){
+      return{
+        
+      }
+    },
+    props: {
+      
+    },
+    methods: {
+      // 去文章详情页
+      toUserProfile(i){
+        this.$router.push({
+          name:'userProfile',
+          query: { id: i }
+        })
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import '@/assets/scss/components/userList';
+</style>
