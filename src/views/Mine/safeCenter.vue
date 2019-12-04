c6e7c2b0005a8fb5242905bd4100c655569fdca5
袁乾峰 <2434740987@qq.com>
2019-12-04 17:25:55 +0800

    创建了安全中心、个性化设置页

---
+++ src/views/Mine/safeCenter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="safeCenter">
+    
+  </div>
+</template>
+
+<script>
+  import ArticleList from '@/components/articleList'
+
+  export default {
+    data(){
+      return {
+        isLoading: false
+      }
+    },
+    components: {
+      ArticleList
+    },
+    methods: {
+      goBack(){
+        this.$router.go(-1)
+      },
+      onRefresh() {
+        setTimeout(() => {
+          this.$toast('刷新成功')
+          this.isLoading = false
+        }, 500)
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import '@/assets/scss/views/safeCenter';
+</style>
