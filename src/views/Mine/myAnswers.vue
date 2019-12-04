d1dd7520b51a719bcf6351dcf4693dee6493912f
袁乾峰 <2434740987@qq.com>
2019-12-04 17:25:18 +0800

    完成我的喜欢、我的问题页

---
+++ src/views/Mine/myAnswers.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="myAnswers">
+    <van-nav-bar
+      fixed="true"
+      left-arrow
+      @click-left="goBack"
+      title="我的回答"/>
+    <div class="container">
+      <div class="answers">
+        <!-- 下拉刷新 -->
+        <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+          <div class="answer-list">
+            <answer-list/>
+          </div>
+        </van-pull-refresh>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import AnswerList from '@/components/answerList'
+
+  export default {
+    data(){
+      return {
+        isLoading: false
+      }
+    },
+    components: {
+      AnswerList
+    },
+    methods: {
+      goBack(){
+        this.$router.go(-1)
+      },
+      onRefresh() {
+        setTimeout(() => {
+          this.$toast('刷新成功')
+          this.isLoading = false
+        }, 500)
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import '@/assets/scss/views/myAnswers';
+</style>
---
+++ src/views/Mine/myLikes.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="myLikes">
+    <van-nav-bar
+      fixed="true"
+      left-arrow
+      @click-left="goBack"
+      title="我的喜欢"/>
+    <div class="container">
+      <div class="articles">
+        <!-- 下拉刷新 -->
+        <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+          <article-list/>
+        </van-pull-refresh>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import ArticleList from '@/components/articleList'
+
+  export default {
+    data(){
+      return {
+        isLoading: false
+      }
+    },
+    components: {
+      ArticleList
+    },
+    methods: {
+      goBack(){
+        this.$router.go(-1)
+      },
+      onRefresh() {
+        setTimeout(() => {
+          this.$toast('刷新成功')
+          this.isLoading = false
+        }, 500)
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import '@/assets/scss/views/myLikes';
+</style>
