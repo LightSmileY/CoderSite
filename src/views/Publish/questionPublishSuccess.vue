0cd1efbfc9e2c58dbf6cacc258633fd8a4b2c4a9
袁乾峰 <2434740987@qq.com>
2019-12-05 14:11:42 +0800

    完成问题发表成功页

--- src/views/Publish/questionPublishSuccess.vue
+++ src/views/Publish/questionPublishSuccess.vue
@@ -1,15 +1,40 @@
 <template>
   <div id="questionPublishSuccess">
-    questionPublishSuccess
+    <van-nav-bar
+      fixed
+      z-index="1000"
+      title="发表问题"
+      left-arrow
+      @click-left="goback"
+    />
+    <div class="container">
+      <div class="success">
+        <img src="@/assets/icons/success.png" alt="">
+        <div>发表成功</div>
+      </div>
+      <div class="title">
+        为您推荐以下用户
+      </div>
+      <user-list answer="true"/>
+    </div>
   </div>
 </template>
 
 <script>
+  import UserList from '@/components/userList'
 
   export default {
     data(){
       return {
 
+      }
+    },
+    components: {
+      UserList
+    },
+    methods: {
+      goback(){
+        this.$router.go(-2)
       }
     }
   };
