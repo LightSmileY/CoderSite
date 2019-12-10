714dd88d86fd4af78cc323bc40e4e6685c2d5a16
袁乾峰 <2434740987@qq.com>
2019-12-10 19:42:50 +0800

    调整首页

--- src/views/Main/index.vue
+++ src/views/Main/index.vue
@@ -47,7 +47,9 @@
           "http://cdn.fengblog.xyz/banner3.png",
           "http://cdn.fengblog.xyz/banner4.png"
         ],
-        isLoading: false
+        isLoading: false,
+        path: 'ws://10.100.238.63:8080/alicluster/server',
+        socket: null
       }
     },
     components: {
@@ -56,14 +58,45 @@
       Extends,
       PostList
     },
+    mounted(){
+      this.init()
+    },
     methods: {
-    onRefresh() {
-      setTimeout(() => {
-        this.$toast('刷新成功')
-        this.isLoading = false
-      }, 500)
+      onRefresh() {
+        setTimeout(() => {
+          this.$toast('刷新成功')
+          this.isLoading = false
+        }, 500)
+      },
+      init: function () {
+       if(typeof(WebSocket) === "undefined"){
+         alert("您的浏览器不支持socket")
+           }else{
+             this.socket = new WebSocket(this.path) // 实例化socket
+             this.socket.onopen = this.open // 监听socket连接
+             this.socket.onerror = this.error // 监听socket错误信息
+             this.socket.onmessage = this.getMessage // 监听socket消息
+             }
+        },
+      open: function () {
+        console.log("socket连接成功")
+      },
+      error: function () {
+         console.log("连接错误")
+      },
+      getMessage: function (msg) {
+         console.log(msg.data)
+      },
+      send: function () {
+        this.socket.send(params)
+      },
+      close: function () {
+        console.log("socket已经关闭")
+      }
+    },
+    destroyed () {  // 销毁监听
+      this.socket.onclose = this.close
     }
-  }
   };
 </script>
 
