6eb880a01b35e440b3e7696743622a2dac0496d9
袁乾峰 <2434740987@qq.com>
2019-12-10 19:43:06 +0800

    完善文章发表页

--- src/views/Publish/pubArticle.vue
+++ src/views/Publish/pubArticle.vue
@@ -8,34 +8,56 @@
       @click-left="goback"
     />
     <div class="container">
+      <!-- 折叠面板标签 -->
+      <van-collapse v-model="activeNames">
+        <van-collapse-item title="标签" name="1">
+          <van-checkbox-group v-model="articleInfo.labels" :max="3">
+            <van-cell-group>
+              <van-cell
+                v-for="(item, index) in labelList"
+                clickable
+                :key="item"
+                :title="`复选框 ${item}`"
+                @click="toggle(index)"
+              >
+                <van-checkbox
+                  :name="item"
+                  ref="checkboxes"
+                  slot="right-icon"
+                />
+              </van-cell>
+            </van-cell-group>
+          </van-checkbox-group>
+        </van-collapse-item>
+      </van-collapse>
       <van-field
-        v-model="message"
+        v-model="articleInfo.title"
         rows="1"
         autosize
-        label="话题"
-        placeholder="请输入话题"
+        label="标题"
+        placeholder="请输入标题"
         clearable
         label-width="60px"
       />
       <!-- 段落 -->
-      <div class="para" v-for="item in 1">
+      <div class="para" v-for="(item, key) in articleInfo.content">
         <van-field
-          v-model="message1"
+          v-model="item.para"
           rows="1"
           autosize
-          :label="'段落'+para"
+          :label="'段落'+(key+1)"
           type="textarea"
           placeholder="请输入内容"
           clearable
           label-width="60px"
         />
         <div class="uploadImage">
-          <van-uploader v-model="imageList" multiple />
+          <van-uploader v-model="item.images" max-count='1'/>
         </div>
       </div>
       <!-- 添加段落 -->
       <div class="addPara">
-        <van-button plain hairline icon="plus" type="primary">添加段落</van-button>
+        <van-button plain hairline icon="plus" type="primary" @click="pushPara">添加段落</van-button>
       </div>
       <!-- 发表帖子 -->
       <div class="post">
@@ -53,12 +75,38 @@
         message: "",
         message1: "",
         para: 1,
-        imageList: []
+        imageList: [],
+        activeNames: [],   //折叠面板控制
+        labelList: ['1','2','3','4','5','6','7','8','9','10','11','12'],
+        articleInfo: {   //文章发表接口参数
+          aid: '',
+          uid: '',
+          postTime: '',
+          labels: [],
+          title: '',
+          content: [
+            {
+              para: '',
+              image: [],  //本地选择
+              image: ''   //实际上传
+            }
+          ]
+        }
       }
     },
     methods: {
       goback(){
         this.$router.go(-1)
+      },
+      toggle(index) {
+        this.$refs.checkboxes[index].toggle();
+      },
+      pushPara(){
+        this.articleInfo.content.push({
+          para: '',
+          images: [],
+          image: ''
+        })
       }
     }
   };
