6a8c5219a66a9c37f1aeb57bfe9d860f271c1c36
袁乾峰 <2434740987@qq.com>
2019-12-13 10:45:57 +0800

    完成登录和注册模块

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,21 +1,48 @@
 <template>
   <div id="login">
-    login
-    <tab-bar/>
+    <div class="container">
+      <van-cell-group>
+        <van-field
+          v-model="userInfo.uid"
+          required
+          clearable
+          label="用户名"
+          placeholder="请输入用户名"
+        />
+        <van-field
+          v-model="userInfo.password"
+          type="password"
+          label="密码"
+          placeholder="请输入密码"
+          required
+        />
+      </van-cell-group>
+      <div class="btn">
+        <van-button plain round hairline size="large" type="primary" @click="signin">登录</van-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import TabBar from '@/components/tabbar'
+  import {login} from '@/api/user'
 
   export default {
     data(){
       return {
-
+        userInfo: {
+          uid: '',
+          password: ''
+        }
       }
     },
-    components: {
-      TabBar
+    methods: {
+      signin(){
+        login(this.userInfo)
+        .then(res => {
+          console.log(res)
+        })
+      }
     }
   };
 </script>
--- src/views/Register/index.vue
+++ src/views/Register/index.vue
@@ -1,15 +1,63 @@
 <template>
   <div id="register">
-    register
+    <div class="container">
+      <van-cell-group>
+        <van-field
+          v-model="userInfo.userId"
+          required
+          clearable
+          label="用户名"
+          placeholder="请输入用户名"
+        />
+        <van-field
+          v-model="userInfo.nickname"
+          required
+          clearable
+          label="昵称"
+          placeholder="请输入昵称"
+        />
+        <van-field
+          v-model="userInfo.password"
+          type="password"
+          label="密码"
+          placeholder="请输入密码"
+          required
+        />
+      </van-cell-group>
+      <div class="btn">
+        <van-button plain round hairline size="large" type="primary" @click="signup">注册</van-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import {register} from '@/api/user'
 
   export default {
     data(){
       return {
-
+        userInfo: {   //注册信息
+          avatarId: "",
+          birthday: "",
+          coverPicture: "",
+          labels: [],
+          mailbox: "",
+          nickname: "",
+          password: "",
+          registerDate: "",
+          sex: true,
+          signature: "",
+          userId: ""
+        }
+      }
+    },
+    methods: {
+      signup(){
+        register(this.userInfo)
+        .then(res => {
+          console.log(res)
+        })
       }
     }
   };
