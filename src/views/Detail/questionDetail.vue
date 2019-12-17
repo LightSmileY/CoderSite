48d5fe1ee86d2bd2e8534f4a277c280614896a2f
袁乾峰 <2434740987@qq.com>
2019-12-17 16:45:43 +0800

    完成基本动态结构

--- src/views/Detail/questionDetail.vue
+++ src/views/Detail/questionDetail.vue
@@ -33,7 +33,10 @@
               </div>
             </div>
             <div class="attent">
-              <van-button 
+              <span v-if="questionInfo.userId == $store.state.userInfo.userId">删除
+              </span>
+              <van-button
+              v-else
               plain 
               hairline 
               round
@@ -62,12 +65,14 @@
               <li>
                 <div class="icon">
                   <van-icon name="like-o" />
+                  <van-icon name="like" />
                 </div>
                 <div class="label">{{questionInfo.likeCount}}</div>
               </li>
               <li>
                 <div class="icon">
                   <van-icon name="star-o" />
+                  <van-icon name="star" />
                 </div>
                 <div class="label">{{questionInfo.collectCount}}</div>
               </li>
@@ -89,7 +94,7 @@
       <div class="wrapper" @click.stop>
         <div class="postMessage">
           <van-field
-            v-model="sms"
+            v-model="commentContent"
             center
             rows="1"
             autosize
@@ -108,15 +113,29 @@
 <script>
   import TabBar from '@/components/tabbar'
   import CommentList from '@/components/commentList'
-  import {getTime} from '@/assets/js/pubfuncs'
-  import {getQuestionById} from '@/api/question'
+  import {getTime, uuid} from '@/assets/js/pubfuncs'
+  import {
+    getQuestionById,  /*获取问题*/
+    deleteQuestionById,
+    addLike,  /*点赞*/
+    deleteLike,
+    addFavorite,  /*收藏*/
+    deleteFavorite,
+    addAnswer,  /*回答*/
+    deleteAnswer
+  } from '@/api/question'
+  import {
+    addFollow,  /*关注*/
+    deleteFollow
+  } from '@/api/user'
 
   export default {
     data(){
       return {
-        isLoading: false,
-        showPostBox: false,
-        questionInfo: {}
+        isLoading: false,    //控制下拉刷新
+        showPostBox: false,  //控制回答输入框
+        questionInfo: {},  //页面内容对象
+        commentContent: ''
       }
     },
     components: {
@@ -133,11 +152,117 @@
           this.isLoading = false
         }, 500)
       },
+      /*传递给点赞等操作的参数*/
+      getAddInfo(param){
+        return {
+          id: uuid(),
+          qid: this.questionInfo.qid,
+          uid: this.questionInfo.userId,
+          content: param,
+          time: new Date()
+        }
+      },
+      /*传递给取消点赞等操作的参数*/
+      getDeleteInfo(param){
+        return {
+          id: param,
+          qid: this.questionInfo.qid
+        }
+      },
+      /*获取问题*/
       getQuestion(){
         getQuestionById({qid: this.$route.query.id})
         .then(res => {
           console.log(res)
           this.questionInfo = res.data.question
+        })
+      },
+      /*删除问题*/
+      deleteQuestion(){
+        deleteQuestionById()
+        .then(res => {
+          console.log(res)
+          this.questionInfo = res.data.question
+        })
+      },
+      /*关注*/
+      follow(){
+        addFollow()
+        .then(res => {
+          console.log(res)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      },
+      /*取消关注*/
+      unFollow(){
+        deleteFollow()
+        .then(res => {
+          console.log(res)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      },
+      /*点赞*/
+      like(){
+        addLike(this.getAddInfo())
+        .then(res => {
+          console.log(res)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      },
+      /*取消点赞*/
+      unLike(param){
+        deleteLike(getDeleteInfo(param))
+        .then(res => {
+          console.log(res)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      },
+      /*收藏*/
+      collect(){
+        addFavorite(this.getAddInfo())
+        .then(res => {
+          console.log(res)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      },
+      /*取消收藏*/
+      unCollect(param){
+        deleteFavorite(getDeleteInfo(param))
+        .then(res => {
+          console.log(res)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      },
+      /*回答*/
+      answer(){
+        addAnswer(this.getAddInfo(this.commentContent))
+        .then(res => {
+          console.log(res)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      },
+      /*删除回答*/
+      unAnswer(param){
+        deleteAnswer(getDeleteInfo(param))
+        .then(res => {
+          console.log(res)
+        })
+        .catch(err => {
+          console.log(err)
         })
       }
     },
